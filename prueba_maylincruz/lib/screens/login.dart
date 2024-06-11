99638884b71333601e2ba30355e779498ba165e5
Maylin Cruz <cruzmaylin157@gmail.com>
2024-06-10 21:00:04 -0600

    Login funcional

---
+++ prueba_maylincruz/lib/screens/login.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late String email;
+  late String password;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _errorMessage = '';
+
+  void _signInWithEmailAndPassword() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      try {
+        final UserCredential userCredential =
+            await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        context.go('/');
+      } catch (e) {
+        setState(() {
+          _errorMessage = 'Error al iniciar sesión: ${e.toString()}';
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio de sesión'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration:
+                        InputDecoration(labelText: 'Correo electrónico'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, ingresa tu correo electrónico';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => email = value!,
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Contraseña'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor, ingresa tu contraseña';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => password = value!,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _signInWithEmailAndPassword,
+                    child: Text('Iniciar sesión'),
+                  ),
+                  SizedBox(height: 10),
+                  if (_errorMessage.isNotEmpty)
+                    Text(
+                      _errorMessage,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  TextButton(
+                    onPressed: () {
+                      context.go('/createUser');
+                    },
+                    child: Text('¿No tienes una cuenta? Crea una aquí'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
